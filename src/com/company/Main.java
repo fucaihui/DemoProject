5a012d3d200dedca5524d6f46ae13526c3d760bc
TerryFu <terryfu68@gmail.com>
2022-02-22 14:18:35 -0500

    nothing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Add Information!");
-        System.out.println("This is a new branch from remote master!");
-        System.out.println("33333!");
     }
 }
