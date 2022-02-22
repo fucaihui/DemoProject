61e097e4d625d2a91265102fceddc9bef8693bec
TerryFu <terryfu68@gmail.com>
2022-02-22 14:07:09 -0500

    Revert "Revert "3!!!""
    This reverts commit 0b1f3e79bcda9c6b3263be0f2ca4c97aaa110d94.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,6 @@ public class Main {
 
         System.out.println("Add Information!");
         System.out.println("This is a new branch from remote master!");
+        System.out.println("33333!");
     }
 }
